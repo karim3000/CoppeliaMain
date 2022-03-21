4869a5185495b3d60dc6c10a265bfcfa2016777c
karim3000 <99026937+karim3000@users.noreply.github.com>
2022-03-21 14:03:31 +0000

    Update Coppelia_main.py

--- Coppelia_main.py
+++ Coppelia_main.py
@@ -15,33 +15,38 @@ accel=40
 jerk=80
 x = 0
 jointHandles = []
-currentConf = []
+currentConf = [0, 0, 0, 0, 0, 0, 0]
 path = '/Franka/joint'
 data = (0, 0)
 
-#sim.simxFinish(-1) # just in case, close all opened connections
+sim.simxFinish(-1) # just in case, close all opened connections
 clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
 
 if clientID!=-1:
     print ('Connected to remote API server')
-    for x in range(7):
-        if x > 0:
-            path = path + '/link' + str(x+1) + '_resp' + '/joint' 
-            print(path)
-        data = sim.simxGetObjectHandle(clientID, path, sim.simx_opmode_oneshot)
-        jointHandles.append(data)
-        print(sim.simxGetObjectHandle(clientID, path, sim.simx_opmode_oneshot))
-    
-    maxVel={vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180}
-    maxAccel={accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180}
-    maxJerk={jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180}
-    targetPos1={90*math.pi/180,90*math.pi/180,135*math.pi/180,-45*math.pi/180,90*math.pi/180,180*math.pi/180,0}
-     
     # for x in range(7):
-    #     currentConf[x]=sim.simxGetJointPosition(clientID, jointHandles[x], sim.simx_opmode_oneshot)
-    #     print(currentConf[x])
+    #     if x > 0:
+    #         path = path + '/link' + str(x+1) + '_resp' + '/joint' 
+    #         print(path)
+    #     data = sim.simxGetObjectHandle(clientID, path, sim.simx_opmode_oneshot)
+    #     jointHandles.append(data)
+    #     print(sim.simxGetObjectHandle(clientID, path, sim.simx_opmode_oneshot))
+    
+    jointHandles = [16, 19, 22, 24, 26, 28, 30]
+    
+    while 1+1 == 2:
+        maxVel={vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180}
+        maxAccel={accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180}
+        maxJerk={jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180}
+        targetPos1={90*math.pi/180,90*math.pi/180,135*math.pi/180,-45*math.pi/180,90*math.pi/180,180*math.pi/180,0}
+         
+        for x in range(6):
+            currentConf[x] = sim.simxGetJointPosition(clientID, jointHandles[x], sim.simx_opmode_oneshot)
+        print(currentConf)
 else:
     print ('Failed connecting to remote API server')
     print ('Program ended')
     sys.exit
+
+
 
