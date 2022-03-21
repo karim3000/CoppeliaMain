cc5ea8d374e1afc06ef3b948f9a87e1f3db2e855
karim3000 <99026937+karim3000@users.noreply.github.com>
2022-03-21 14:45:45 +0000

    Update Coppelia_main.py

--- Coppelia_main.py
+++ Coppelia_main.py
@@ -27,26 +27,29 @@ if clientID!=-1:
     # for x in range(7):
     #     if x > 0:
     #         path = path + '/link' + str(x+1) + '_resp' + '/joint' 
-    #         print(path)
+    #         # print(path)
     #     data = sim.simxGetObjectHandle(clientID, path, sim.simx_opmode_oneshot)
-    #     jointHandles.append(data)
-    #     print(sim.simxGetObjectHandle(clientID, path, sim.simx_opmode_oneshot))
+    #     jointHandles.append(data[0])
+    #     print(data)
     
     jointHandles = [16, 19, 22, 24, 26, 28, 30]
     
     while 1+1 == 2:
-        maxVel={vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180}
-        maxAccel={accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180}
-        maxJerk={jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180}
-        targetPos1={90*math.pi/180,90*math.pi/180,135*math.pi/180,-45*math.pi/180,90*math.pi/180,180*math.pi/180,0}
+        # maxVel={vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180,vel*math.pi/180}
+        # maxAccel={accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180,accel*math.pi/180}
+        # maxJerk={jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180,jerk*math.pi/180}
+        # targetPos1={90*math.pi/180,90*math.pi/180,135*math.pi/180,-45*math.pi/180,90*math.pi/180,180*math.pi/180,0}
          
         for x in range(6):
             currentConf[x] = sim.simxGetJointPosition(clientID, jointHandles[x], sim.simx_opmode_oneshot)
         print(currentConf)
+        sim.simxSetJointTargetPosition(clientID, 28, 2.8, sim.simx_opmode_oneshot)
+        sim.simxSetJointTargetPosition(clientID, 26, 1.24, sim.simx_opmode_oneshot)
+        sim.simxSetJointTargetPosition(clientID, 24, -1, sim.simx_opmode_oneshot)
+        
+        
 else:
     print ('Failed connecting to remote API server')
     print ('Program ended')
     sys.exit
-
-
 
